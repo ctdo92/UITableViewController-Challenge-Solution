5a1756dbe13d805d0122f3104730ab9ad7ff4acc
Chuong Do <chuongdo@Chuongs-MacBook-Air.local>
2017-05-06 22:27:47 -0500

    solved the challenge

--- UITableViewController Challenge Solution/TableViewController.h
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Chuong Do on 5/6/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
